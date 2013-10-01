2f27f893242c129b21b96bf8422fc51ce79f4751
suresh@ubuntu-desktop <suresh.sivanantham@gmail.com>
2013-10-01 21:00:11 +0530

    logger custom tag creation started + src folders updated for all

---
+++ webAppTmpl/jspUtils/src/main/java/in/v8delta/template/myWebAppTmpl/tags/log/LoggerTag.java
@@ -0,0 +1,23 @@
+package in.v8delta.template.myWebAppTmpl.tags.log;
+
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+/**
+ * 
+ * Custom tag class for writing logs from within a jsp file.
+ * 
+ * <pre>
+ * Usage : 
+ * -------
+ * 
+ * 
+ * 
+ * </pre>
+ * 
+ * @author v8-suresh
+ *
+ */
+@SuppressWarnings("serial")
+public class LoggerTag extends BodyTagSupport{
+
+}
