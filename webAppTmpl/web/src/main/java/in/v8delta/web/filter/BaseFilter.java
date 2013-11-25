be083e08be4aa3b9867edf2ca1a2d3110e14a291
suresh@win7-desktop <cliksuresh18@gmail.com>
2013-11-25 11:43:50 +0530

    Base Filter Created successfully... Now time to test with customized filters..

--- webAppTmpl/web/src/main/java/in/v8delta/web/filter/BaseFilter.java
+++ webAppTmpl/web/src/main/java/in/v8delta/web/filter/BaseFilter.java
@@ -1,10 +1,8 @@
 package in.v8delta.web.filter;
 
 import in.v8delta.template.myWebAppTmpl.core.log.LoggerAgent;
-<<<<<<< HEAD
 import in.v8delta.template.myWebAppTmpl.core.utils.AppConstants;
 import in.v8delta.template.myWebAppTmpl.core.utils.LogUtil;
-import in.v8delta.web.controller.async.AppConfigControllerAsync;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -14,7 +12,6 @@ import java.util.regex.Pattern;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -245,57 +242,4 @@ public abstract class BaseFilter implements Filter {
 	protected LoggerAgent getLogger() {
 		return LOGGER;
 	}
-
-=======
-import in.v8delta.template.myWebAppTmpl.core.utils.LogUtil;
-import in.v8delta.web.controller.async.AppConfigControllerAsync;
-
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-
-/**
- * 
- * Base Filter for All the Application Specific Filters
- * 
- * @author v8-suresh
- *
- */
-public class BaseFilter implements Filter{
-
-	private final static LoggerAgent LOGGER   = LogUtil.getAppLogger(BaseFilter.class);
-	
-	private boolean isEnabled;
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		if(getLogger().isDebugEnabled()){
-			getLogger().debug("");
-		}
-	}
-
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		
-	}
-
-	@Override
-	public void destroy() {
-		if(getLogger().isDebugEnabled()){
-			getLogger().debug("");
-		}
-		
-	}
-	
-	protected LoggerAgent getLogger(){
-		return LOGGER;
-	}
-	
->>>>>>> refs/remotes/origin/master
 }
