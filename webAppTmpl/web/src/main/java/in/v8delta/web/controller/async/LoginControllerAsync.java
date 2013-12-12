0c1305b05952d4e517fb2c30495cfa8adf454fc0
suresh@win7-desktop <cliksuresh18@gmail.com>
2013-12-12 20:48:38 +0530

    Few more on Hibernate

--- webAppTmpl/web/src/main/java/in/v8delta/web/controller/async/LoginControllerAsync.java
+++ webAppTmpl/web/src/main/java/in/v8delta/web/controller/async/LoginControllerAsync.java
@@ -1,5 +1,9 @@
 package in.v8delta.web.controller.async;
 
+import in.v8delta.service.UserDaoService;
+import in.v8delta.service.spi.UserDaoServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginControllerAsync {
+
+	@Autowired
+	UserDaoService userDao;
 	
 	@RequestMapping(value = "loginService", method = RequestMethod.POST)
 	public @ResponseBody String doUserLogin(){
+		
 		return "{success: true," +
 					"user: {" +
 						"'name': 'Suresh'," +
